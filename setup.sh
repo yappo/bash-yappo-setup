4c739dd35a010300796bd02ff6ba6f2a4d7713ea
Kazuhiro Osawa <yappo@shibuya.pl>
2011-08-25 13:17:32 +0900

    auto downloader

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# download
+cd /tmp
+tmpname=`head -n 10 /dev/random | strings | perl -e 'my $str; while(<>){$str.=$_}$str =~ s/\W//g; $str =~ s/^(.{,32}).*$/$1/; print $str'`
+echo $tmpname
+git clone git://github.com/yappo/bash-yappo-setup.git $tmpname
+cd $tmpname
+
+# copy files
 for path in `ls -a`; do
     if [ $path = "setup.sh" -o $path = "." -o $path = ".." ]; then
 	continue;
@@ -27,3 +35,7 @@ if [ -e "$HOME/perl5/perlbrew/etc/bashrc" ]; then
 elif [ -e "$HOME/perl5/etc/bashrc" ]; then
     echo "source $HOME/perl5/etc/bashrc" >> "$HOME/.bashrc"
 fi
+
+# cleanup
+cd /tmp
+rm -fr $tmpname
